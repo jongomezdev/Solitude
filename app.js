03f066e1b1f2f8d63cd0be380c5482ce8f00d91a
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-19 16:31:59 -0500

    install and test ejsMate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Slope = require("./models/slope");
 
@@ -18,6 +19,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
