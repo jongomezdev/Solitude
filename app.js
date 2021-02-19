6ded882f857588c4a442ea2a310bcfc1c8ce01b1
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-19 13:10:48 -0500

    require method override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
 const Slope = require("./models/slope");
 
 mongoose.connect("mongodb://localhost:27017/solitude-db", {
