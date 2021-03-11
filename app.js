3270b0bd8b1fc49e3cbbf9cdcd6eba750e8872c2
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-11 15:36:08 -0500

    add utility to all async routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Slope = require("./models/slope");
 
@@ -29,46 +30,61 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("Home");
 });
-app.get("/slopes", async (req, res) => {
-  const slopes = await Slope.find({});
-  res.render("slopes/index", { slopes });
-});
+
+app.get(
+  "/slopes",
+  catchAsync(async (req, res) => {
+    const slopes = await Slope.find({});
+    res.render("slopes/index", { slopes });
+  })
+);
 
 app.get("/slopes/new", (req, res) => {
   res.render("slopes/new");
 });
 
-app.post("/slopes", async (req, res, next) => {
-  try {
+app.post(
+  "/slopes",
+  catchAsync(async (req, res, next) => {
     const slope = new Slope(req.body.slope);
     await slope.save();
     res.redirect(`/slopes/${slope._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
-app.get("/slopes/:id", async (req, res) => {
-  const slope = await Slope.findById(req.params.id);
-  res.render("slopes/show", { slope });
-});
+app.get(
+  "/slopes/:id",
+  catchAsync(async (req, res) => {
+    const slope = await Slope.findById(req.params.id);
+    res.render("slopes/show", { slope });
+  })
+);
 
-app.get("/slopes/:id/edit", async (req, res) => {
-  const slope = await Slope.findById(req.params.id);
-  res.render("slopes/edit", { slope });
-});
+app.get(
+  "/slopes/:id/edit",
+  catchAsync(async (req, res) => {
+    const slope = await Slope.findById(req.params.id);
+    res.render("slopes/edit", { slope });
+  })
+);
 
-app.put("/slopes/:id", async (req, res) => {
-  const { id } = req.params;
-  const slope = await Slope.findByIdAndUpdate(id, { ...req.body.slope });
-  res.redirect(`/slopes/${slope._id}`);
-});
+app.put(
+  "/slopes/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const slope = await Slope.findByIdAndUpdate(id, { ...req.body.slope });
+    res.redirect(`/slopes/${slope._id}`);
+  })
+);
 
-app.delete("/slopes/:id", async (req, res) => {
-  const { id } = req.params;
-  await Slope.findByIdAndDelete(id);
-  res.redirect("/slopes");
-});
+app.delete(
+  "/slopes/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Slope.findByIdAndDelete(id);
+    res.redirect("/slopes");
+  })
+);
 
 app.use((err, req, res, next) => {
   res.send("Something went wrong!");
